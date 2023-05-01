369bdfeb8040ebbfc9125b1954aa4ee65448c90c
rovshan <rouvsen@gmail.com>
2023-05-01 14:48:37 +0400

    main class added

---
+++ src/main/java/com/rouvsen/Application.java
@@ -0,0 +1,21 @@
+package com.rouvsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+//		ConfigurableApplicationContext context =
+				SpringApplication.run(Application.class, args);
+//		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+//		RestTemplate restTemplate = (RestTemplate)context.getBean("restTemplate");
+//		System.out.println(restTemplate);
+	}
+
+}
