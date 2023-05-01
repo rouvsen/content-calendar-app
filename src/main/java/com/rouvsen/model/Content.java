75f6606c4657cf8d4e77b4b1abd663bb1f4d080d
rovshan <rouvsen@gmail.com>
2023-05-01 14:48:53 +0400

    Content Record (class) added

---
+++ src/main/java/com/rouvsen/model/Content.java
@@ -0,0 +1,19 @@
+package com.rouvsen.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.time.LocalDateTime;
+
+public record Content(
+        Integer id,
+        @NotBlank
+        String title,
+        String desc,
+        Status status,
+        Type contentType,
+        LocalDateTime dateCreated,
+        LocalDateTime dateUpdated,
+        String url
+) {
+}
