37856dd852928ed702ab596d480dcba014ba9e1b
rovshan <rouvsen@gmail.com>
2023-05-02 23:32:30 +0400

    ContentController updated

--- src/main/java/com/rouvsen/controller/ContentController.java
+++ src/main/java/com/rouvsen/controller/ContentController.java
@@ -1,8 +1,10 @@
 package com.rouvsen.controller;
 
 import com.rouvsen.model.Content;
+import com.rouvsen.model.Status;
 import com.rouvsen.repository.ContentCollectionRepository;
 import com.rouvsen.repository.ContentJdbcTemplateRepository;
+import com.rouvsen.repository.ContentRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +17,10 @@ import java.util.List;
 @CrossOrigin
 public class ContentController {
 
-    private final ContentCollectionRepository repository;
+    private final ContentRepository repository;
 //    private final ContentJdbcTemplateRepository repository;
 
-    public ContentController(ContentCollectionRepository repository) {
+    public ContentController(ContentRepository repository) {
         this.repository = repository;
     }
 
@@ -57,7 +59,17 @@ public class ContentController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("{id}")
     public void delete(@PathVariable Integer id) {
-        repository.delete(id);
+        repository.deleteById(id);
+    }
+
+    @GetMapping("/filter/{keyword}")
+    public List<Content> findByTitle(@PathVariable String keyword) {
+        return repository.findAllByTitleContains(keyword);
+    }
+
+    @GetMapping("/filter/status/{status}")
+    public List<Content> findByStatus(@PathVariable Status status) {
+        return repository.listByStatus(status);
     }
 
 }
