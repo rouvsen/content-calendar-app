354a79002a8bd95527c6758f1e2c90d03db98a62
rovshan <rouvsen@gmail.com>
2023-05-02 23:26:29 +0400

    Content model changes added

--- src/main/java/com/rouvsen/model/Content.java
+++ src/main/java/com/rouvsen/model/Content.java
@@ -2,10 +2,13 @@ package com.rouvsen.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDateTime;
 
 public record Content(
+        @Id
         Integer id,
         @NotBlank
         String title,
