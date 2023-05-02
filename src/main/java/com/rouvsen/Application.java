f3a6c1ad33759d8428ae63253a1ac1969e850c5f
rovshan <rouvsen@gmail.com>
2023-05-02 23:25:57 +0400

    @ConfigurationProperties for activating properties

--- src/main/java/com/rouvsen/Application.java
+++ src/main/java/com/rouvsen/Application.java
@@ -1,12 +1,22 @@
 package com.rouvsen;
 
+import com.rouvsen.config.ContentCalendarProperties;
+import com.rouvsen.model.Content;
+import com.rouvsen.model.Status;
+import com.rouvsen.model.Type;
+import com.rouvsen.repository.ContentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
+@EnableConfigurationProperties(value = ContentCalendarProperties.class)
 @SpringBootApplication
 public class Application {
 
@@ -17,5 +27,21 @@ public class Application {
 //		RestTemplate restTemplate = (RestTemplate)context.getBean("restTemplate");
 //		System.out.println(restTemplate);
 	}
+
+//	@Bean
+//	CommandLineRunner commandLineRunner(ContentRepository repository){
+//		return args -> {
+//			Content content = new Content(null,
+//					"First Title - Chat Gpt",
+//					"First Description - Chat Gpt",
+//					Status.IDEA,
+//					Type.ARTICLE,
+//					LocalDateTime.now(),
+//					null,
+//					"");
+//			repository.save(content);
+//		};
+//
+//	}
 
 }
