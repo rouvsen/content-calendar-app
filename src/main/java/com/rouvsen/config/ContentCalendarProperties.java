37cd80d9d7ebf97de37e6ec6dc026fd52fd6f75f
rovshan <rouvsen@gmail.com>
2023-05-02 23:27:15 +0400

    ContentCalendarProperties, properties @Component

---
+++ src/main/java/com/rouvsen/config/ContentCalendarProperties.java
@@ -0,0 +1,10 @@
+package com.rouvsen.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(value = "cc")
+public record ContentCalendarProperties(
+        String welcomeMessage,
+        String about) {
+
+}
